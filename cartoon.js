54f105ea43f21753cb2ed465285978ab120c2221
anastassiya.savostina <a.savostina@icloud.com>
2022-04-17 00:29:45 -0600

    done

---
+++ cartoon.js
@@ -0,0 +1,243 @@
+const apiURL ="https://www.omdbapi.com/?apikey=6c3f9ab7&i=tt6467266";
+
+fetch (apiURL)
+.then((response) => response.json())
+.then((movieInfo)=> {
+    console.log(movieInfo);
+
+document.getElementById("actors").innerHTML=movieInfo.Actors;
+document.getElementById("director").innerHTML=movieInfo.Director;
+document.getElementById("released").innerHTML=movieInfo.Released;
+document.getElementById("plot").innerHTML=movieInfo.Plot;
+document.getElementById("awards").innerHTML=movieInfo.Awards;
+document.getElementById("runtime").innerHTML=movieInfo.Runtime;
+}
+);
+
+const api2URL ="https://www.omdbapi.com/?apikey=6c3f9ab7&i=tt12412888";
+
+fetch (api2URL)
+.then((response) => response.json())
+.then((movieInfo)=> {
+    console.log(movieInfo);
+
+document.getElementById("actors2").innerHTML=movieInfo.Actors;
+document.getElementById("director2").innerHTML=movieInfo.Director;
+document.getElementById("released2").innerHTML=movieInfo.Released;
+document.getElementById("plot2").innerHTML=movieInfo.Plot;
+document.getElementById("country").innerHTML=movieInfo.Country;
+document.getElementById("year2").innerHTML=movieInfo.Year;
+}
+);
+
+const api3URL ="https://www.omdbapi.com/?apikey=6c3f9ab7&i=tt1596342";
+
+fetch (api3URL)
+.then((response) => response.json())
+.then((movieInfo)=> {
+    console.log(movieInfo);
+
+document.getElementById("actors3").innerHTML=movieInfo.Actors;
+document.getElementById("director3").innerHTML=movieInfo.Director;
+document.getElementById("plot3").innerHTML=movieInfo.Plot;
+document.getElementById("country3").innerHTML=movieInfo.Country;
+document.getElementById("year").innerHTML=movieInfo.Year;
+}
+);
+
+// Page 2
+const api4URL ="https://www.omdbapi.com/?apikey=6c3f9ab7&i=tt4520988";
+
+fetch (api4URL)
+.then((response) => response.json())
+.then((movieInfo)=> {
+    console.log(movieInfo);
+
+document.getElementById("actors4").innerHTML=movieInfo.Actors;
+document.getElementById("director4").innerHTML=movieInfo.Director;
+document.getElementById("awards4").innerHTML=movieInfo.Awards;
+document.getElementById("plot4").innerHTML=movieInfo.Plot;
+document.getElementById("runtime4").innerHTML=movieInfo.Runtime;
+document.getElementById("released4").innerHTML=movieInfo.Released;
+}
+);
+
+const api5URL ="https://www.omdbapi.com/?apikey=6c3f9ab7&i=tt5848272";
+
+fetch (api5URL)
+.then((response) => response.json())
+.then((movieInfo)=> {
+    console.log(movieInfo);
+
+document.getElementById("actors5").innerHTML=movieInfo.Actors;
+document.getElementById("director5").innerHTML=movieInfo.Director;
+document.getElementById("awards5").innerHTML=movieInfo.Awards;
+document.getElementById("plot5").innerHTML=movieInfo.Plot;
+document.getElementById("runtime5").innerHTML=movieInfo.Runtime;
+document.getElementById("released5").innerHTML=movieInfo.Released;
+}
+);
+
+const api6URL ="https://www.omdbapi.com/?apikey=6c3f9ab7&i=tt2245084";
+
+fetch (api6URL)
+.then((response) => response.json())
+.then((movieInfo)=> {
+    console.log(movieInfo);
+
+document.getElementById("actors6").innerHTML=movieInfo.Actors;
+document.getElementById("director6").innerHTML=movieInfo.Director;
+document.getElementById("awards6").innerHTML=movieInfo.Awards;
+document.getElementById("plot6").innerHTML=movieInfo.Plot;
+document.getElementById("runtime6").innerHTML=movieInfo.Runtime;
+document.getElementById("released6").innerHTML=movieInfo.Released;
+}
+);
+
+// PAGE 3
+const api7URL ="https://www.omdbapi.com/?apikey=6c3f9ab7&i=tt1979376";
+
+fetch (api7URL)
+.then((response) => response.json())
+.then((movieInfo)=> {
+    console.log(movieInfo);
+
+document.getElementById("actors7").innerHTML=movieInfo.Actors;
+document.getElementById("director7").innerHTML=movieInfo.Director;
+document.getElementById("awards7").innerHTML=movieInfo.Awards;
+document.getElementById("plot7").innerHTML=movieInfo.Plot;
+document.getElementById("runtime7").innerHTML=movieInfo.Runtime;
+document.getElementById("released7").innerHTML=movieInfo.Released;
+}
+);
+
+const api8URL ="https://www.omdbapi.com/?apikey=6c3f9ab7&i=tt0382932";
+
+fetch (api8URL)
+.then((response) => response.json())
+.then((movieInfo)=> {
+    console.log(movieInfo);
+
+document.getElementById("actors8").innerHTML=movieInfo.Actors;
+document.getElementById("director8").innerHTML=movieInfo.Director;
+document.getElementById("awards8").innerHTML=movieInfo.Awards;
+document.getElementById("plot8").innerHTML=movieInfo.Plot;
+document.getElementById("runtime8").innerHTML=movieInfo.Runtime;
+document.getElementById("released8").innerHTML=movieInfo.Released;
+}
+);
+
+const api9URL ="https://www.omdbapi.com/?apikey=6c3f9ab7&i=tt2096673";
+
+fetch (api9URL)
+.then((response) => response.json())
+.then((movieInfo)=> {
+    console.log(movieInfo);
+
+document.getElementById("actors9").innerHTML=movieInfo.Actors;
+document.getElementById("director9").innerHTML=movieInfo.Director;
+document.getElementById("awards9").innerHTML=movieInfo.Awards;
+document.getElementById("plot9").innerHTML=movieInfo.Plot;
+document.getElementById("runtime9").innerHTML=movieInfo.Runtime;
+document.getElementById("released9").innerHTML=movieInfo.Released;
+}
+);
+
+// Page 4
+const api10URL ="https://www.omdbapi.com/?apikey=6c3f9ab7&i=tt0892791";
+
+fetch (api10URL)
+.then((response) => response.json())
+.then((movieInfo)=> {
+    console.log(movieInfo);
+
+document.getElementById("actors10").innerHTML=movieInfo.Actors;
+document.getElementById("director10").innerHTML=movieInfo.Director;
+document.getElementById("awards10").innerHTML=movieInfo.Awards;
+document.getElementById("plot10").innerHTML=movieInfo.Plot;
+document.getElementById("runtime10").innerHTML=movieInfo.Runtime;
+document.getElementById("released10").innerHTML=movieInfo.Released;
+}
+);
+
+const api11URL ="https://www.omdbapi.com/?apikey=6c3f9ab7&i=tt1277953";
+
+fetch (api11URL)
+.then((response) => response.json())
+.then((movieInfo)=> {
+    console.log(movieInfo);
+
+document.getElementById("actors11").innerHTML=movieInfo.Actors;
+document.getElementById("director11").innerHTML=movieInfo.Director;
+document.getElementById("awards11").innerHTML=movieInfo.Awards;
+document.getElementById("plot11").innerHTML=movieInfo.Plot;
+document.getElementById("runtime11").innerHTML=movieInfo.Runtime;
+document.getElementById("released11").innerHTML=movieInfo.Released;
+}
+);
+
+
+const api12URL ="https://www.omdbapi.com/?apikey=6c3f9ab7&i=tt2386490";
+fetch(api12URL)
+.then((response) => response.json())
+.then((movieInfo)=> {
+    console.log(movieInfo);
+
+document.getElementById("actors12").innerHTML=movieInfo.Actors;
+document.getElementById("director12").innerHTML=movieInfo.Director;
+document.getElementById("awards12").innerHTML=movieInfo.Awards;
+document.getElementById("plot12").innerHTML=movieInfo.Plot;
+document.getElementById("runtime12").innerHTML=movieInfo.Runtime;
+document.getElementById("released12").innerHTML=movieInfo.Released;
+}
+);
+
+// Page 5
+const api13URL ="https://www.omdbapi.com/?apikey=6c3f9ab7&i=tt2948372";
+
+fetch (api13URL)
+.then((response) => response.json())
+.then((movieInfo)=> {
+    console.log(movieInfo);
+
+document.getElementById("actors13").innerHTML=movieInfo.Actors;
+document.getElementById("director13").innerHTML=movieInfo.Director;
+document.getElementById("awards13").innerHTML=movieInfo.Awards;
+document.getElementById("plot13").innerHTML=movieInfo.Plot;
+document.getElementById("runtime13").innerHTML=movieInfo.Runtime;
+document.getElementById("released13").innerHTML=movieInfo.Released;
+}
+);
+
+const api14URL ="https://www.omdbapi.com/?apikey=6c3f9ab7&i=tt7979580";
+
+fetch (api14URL)
+.then((response) => response.json())
+.then((movieInfo)=> {
+    console.log(movieInfo);
+
+document.getElementById("actors14").innerHTML=movieInfo.Actors;
+document.getElementById("director14").innerHTML=movieInfo.Director;
+document.getElementById("awards14").innerHTML=movieInfo.Awards;
+document.getElementById("plot14").innerHTML=movieInfo.Plot;
+document.getElementById("runtime14").innerHTML=movieInfo.Runtime;
+document.getElementById("released14").innerHTML=movieInfo.Released;
+}
+);
+
+
+const api15URL ="https://www.omdbapi.com/?apikey=6c3f9ab7&i=tt12801262";
+fetch(api15URL)
+.then((response) => response.json())
+.then((movieInfo)=> {
+    console.log(movieInfo);
+
+document.getElementById("actors15").innerHTML=movieInfo.Actors;
+document.getElementById("director15").innerHTML=movieInfo.Director;
+document.getElementById("awards15").innerHTML=movieInfo.Awards;
+document.getElementById("plot15").innerHTML=movieInfo.Plot;
+document.getElementById("runtime15").innerHTML=movieInfo.Runtime;
+document.getElementById("released15").innerHTML=movieInfo.Released;
+}
+);
+
